b4565444747eeb9d29ca6bef4490122866c89ad8
Pedro Alves <185304469+alvespedro0225@users.noreply.github.com>
2025-12-04 22:18:44 -0300

    fix: changed base configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="vitest/config"/>
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
 });
